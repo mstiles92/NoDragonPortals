d8969c5f2439b09f9b240d36c94439b1a7219a1c
mstiles92 <mstiles92@gmail.com>
2013-12-02 11:31:15 -0500

    Code cleanup

--- src/main/java/com/mstiles92/plugins/nodragonportals/NoDragonPortals.java
+++ src/main/java/com/mstiles92/plugins/nodragonportals/NoDragonPortals.java
@@ -1,15 +1,20 @@
 package com.mstiles92.plugins.nodragonportals;
 
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class NoDragonPortals extends JavaPlugin {
+    private static Plugin plugin;
 
 	public void onEnable() {
-		new PortalCreationListener(this);
-		this.getConfig().options().copyDefaults(true);
-		this.saveConfig();
+        plugin = this;
+
+        saveDefaultConfig();
+
+        getServer().getPluginManager().registerEvents(new PortalCreationListener(), this);
 	}
 	
-	public void onDisable() {
-	}
+	public static Plugin getPlugin() {
+        return plugin;
+    }
 }
--- src/main/java/com/mstiles92/plugins/nodragonportals/PortalCreationListener.java
+++ src/main/java/com/mstiles92/plugins/nodragonportals/PortalCreationListener.java
@@ -1,51 +1,46 @@
 package com.mstiles92.plugins.nodragonportals;
 
-import java.util.List;
-
 import org.bukkit.Material;
 import org.bukkit.PortalType;
 import org.bukkit.block.BlockState;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityCreatePortalEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.inventory.ItemStack;
 
 public class PortalCreationListener implements Listener {
-
-	private final NoDragonPortals plugin;
 	
-	public PortalCreationListener(NoDragonPortals plugin) {
-		plugin.getServer().getPluginManager().registerEvents(this, plugin);
-		this.plugin = plugin;
-	}
-	
-	@EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
+	@EventHandler(ignoreCancelled = true)
 	public void onEntityCreatePortalEvent(EntityCreatePortalEvent event) {
-		if ((event.getEntityType() == EntityType.ENDER_DRAGON) && (event.getPortalType() == PortalType.ENDER) && (plugin.getConfig().getBoolean("DisablePortals"))) {
+        FileConfiguration config = NoDragonPortals.getPlugin().getConfig();
+
+		if ((event.getEntityType() == EntityType.ENDER_DRAGON) && (event.getPortalType() == PortalType.ENDER) && (config.getBoolean("DisablePortals"))) {
 			event.setCancelled(true);
 		}
+
 		// Remove egg from top of portal if portals are not disabled and giving egg to player
-		if ((!plugin.getConfig().getBoolean("DisablePortals")) && (plugin.getConfig().getBoolean("GiveEggToPlayer"))) {
-			List<BlockState> blocks = event.getBlocks();
-			for (BlockState bs : blocks) {
-				if (bs.getType() == Material.DRAGON_EGG) {
-					bs.setType(Material.AIR);
-					bs.update();
+		if ((!config.getBoolean("DisablePortals")) && (config.getBoolean("GiveEggToPlayer"))) {
+			for (BlockState state : event.getBlocks()) {
+				if (state.getType() == Material.DRAGON_EGG) {
+					state.setType(Material.AIR);
+					state.update();
 				}
 			}
 		}
 	}
 	
-	@EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
+	@EventHandler(ignoreCancelled = true)
 	public void onEntityDeathEvent(EntityDeathEvent event) {
-		if ((event.getEntityType() == EntityType.ENDER_DRAGON) && (plugin.getConfig().getBoolean("GiveEggToPlayer"))) {
-			Player p = event.getEntity().getKiller();
-			ItemStack i = new ItemStack(Material.DRAGON_EGG, 1);
-			p.getInventory().addItem(i);
+        FileConfiguration config = NoDragonPortals.getPlugin().getConfig();
+
+		if ((event.getEntityType() == EntityType.ENDER_DRAGON) && (config.getBoolean("GiveEggToPlayer"))) {
+			Player player = event.getEntity().getKiller();
+			ItemStack item = new ItemStack(Material.DRAGON_EGG, 1);
+			player.getInventory().addItem(item);
 		}
 	}
 }
